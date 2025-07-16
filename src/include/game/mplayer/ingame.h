bf6f69fb7c6fbcba387b79ad3e045af45d20022f
Morrigan Aensland-Hill <98400773+MorriganAensland-Hill@users.noreply.github.com>
2025-07-16 17:20:14 -0400

    Add files via upload

--- src/include/game/mplayer/ingame.h
+++ src/include/game/mplayer/ingame.h
@@ -4,11 +4,26 @@
 #include "data.h"
 #include "types.h"
 
-char *menutext_pause_or_unpause(s32 arg0);
-char *mp_menu_text_weapon_of_choice_name(struct menuitem *item);
-char *mp_menu_text_player_title(s32 arg0);
-void mp_push_pause_dialog(void);
-void mp_push_endscreen_dialog(u32 arg0, u32 playernum);
-MenuItemHandlerResult menuhandler_mp_pause(s32 operation, struct menuitem *item, union handlerdata *data);
+MenuItemHandlerResult mpStatsForPlayerDropdownHandler(s32 operation, struct menuitem *item, union handlerdata *data);
+char *mpMenuTextInGameLimit(struct menuitem *item);
+char *menutextPauseOrUnpause(s32 arg0);
+char *menutextMatchTime(s32 arg0);
+char *mpMenuTextWeaponDescription(struct menuitem *item);
+char *mpMenuTitleStatsFor(struct menudialogdef *dialogdef);
+char *mpMenuTextWeaponOfChoiceName(struct menuitem *item);
+char *mpMenuTextAward1(struct menuitem *item);
+char *mpMenuTextAward2(struct menuitem *item);
+char *mpMenuTextPlacementWithSuffix(struct menuitem *item);
+MenuItemHandlerResult mpPlacementMenuHandler(s32 operation, struct menuitem *item, union handlerdata *data);
+MenuItemHandlerResult mpAwardsMenuHandler(s32 operation, struct menuitem *item, union handlerdata *data);
+MenuItemHandlerResult mpPlayerTitleMenuHandler(s32 operation, struct menuitem *item, union handlerdata *data);
+char *mpMenuTextPlayerTitle(s32 arg0);
+MenuItemHandlerResult mpConfirmPlayerNameHandler(s32 operation, struct menuitem *item, union handlerdata *data);
+void mpPushPauseDialog(void);
+void mpPushEndscreenDialog(u32 arg0, u32 playernum);
+MenuItemHandlerResult menuhandlerMpEndGame(s32 operation, struct menuitem *item, union handlerdata *data);
+MenuItemHandlerResult menuhandler00178018(s32 operation, struct menuitem *item, union handlerdata *data);
+MenuItemHandlerResult menuhandlerMpInGameLimitLabel(s32 operation, struct menuitem *item, union handlerdata *data);
+MenuItemHandlerResult menuhandlerMpPause(s32 operation, struct menuitem *item, union handlerdata *data);
 
 #endif
